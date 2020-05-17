ec33d4ef8a07ac00b35c55e92b85e738c59b9944
iTi1337 <64363659+iTi1337@users.noreply.github.com>
2020-05-17 20:35:41 +0200

    Added a testcase when executing the drawmanager.py

--- src/drawmanager/drawmanager.py
+++ src/drawmanager/drawmanager.py
@@ -1,3 +1,4 @@
+from datamanager import DataManager
 import pandas as pd
 import numpy as np
 import plotly.express as px
@@ -6,7 +7,7 @@ class DrawManager():
     def __init__(self):
         pass
 
-    def create_fig(self, df, time_scope, df_attribute = []):
+    def create_fig(self, df, df_attribute = []):
         if df_attribute == []:
             fig = px.line(df, x="time", y=df.columns[1])
         else:
@@ -14,5 +15,11 @@ class DrawManager():
             fig = px.line(df_melt, x="time" , y="value")
         return fig
 
-if __name__ == __main__:
-    pass
+if __name__ == "__main__":
+    data_m = DataManager()
+    file = "C:/Users/tobia/OneDrive/Documents/GitHub/PA1450-Group-7-Online-Weather/downloads/smhi-opendata_3_4_65090_20200512_194409.csv"
+    data_m.load_dataframe(file)
+    df = data_m.get_category(data_m.categories[0])
+    draw_m = DrawManager()
+    fig = draw_m.create_fig(df)
+    fig.write_html('first_figure.html', auto_open=True)
