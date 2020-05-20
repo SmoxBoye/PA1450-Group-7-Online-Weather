4a8cc72c7d290f5d64d8e2ed2f1576fe3cf72050
Jesper <jesper.ls@hotmail.com>
2020-05-20 16:47:31 +0200

    Switching categories now works
    Added functionality to the category dropdown

--- www/application/routes.py
+++ www/application/routes.py
@@ -1,7 +1,7 @@
 """Core Flask app routes."""
 from flask import render_template
 from flask import current_app as app
-from flask import request, redirect, flash, url_for
+from flask import request, redirect, flash, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 import os
 from .drawmanager import DrawManager
@@ -18,32 +18,36 @@ def allowed_file(filename):
 
 @app.route('/', methods=["GET", "POST"])
 def home():
-    dm = DrawManager()
-    df = DataManager()
+    draw = DrawManager()
+    data = DataManager()
     """Landing page."""
     if request.method == 'POST':
         # check if the post request has the file part
-        print("DAB")
+        print("1")
         if 'file' in request.files:
             print("2")
             file = request.files['file']
             if file.filename != '' and allowed_file(file.filename):
                 print("3")
                 file.save("data/data.csv")
-                print(df.load_dataframe("data/data.csv"))
-                print(len(df.categories))
-                category = df.categories[0]
-                dataf = df.get_category(category)
-                print(dataf)
-                fig = dm.create_fig(dataf)
-                fig.write_html('application/templates/' + str(category) + '.html', auto_open=False)
-
+                print(data.load_dataframe("data/data.csv"))
+                print(len(data.categories))
+                category = data.categories[0]
+                df = data.get_category(category)
+                #print(df)
+                fig = draw.create_fig(df)
+                fig.write_html('application/templates/categories/' + str(category) + '.html', auto_open=False)
+                file.save("data/" + str(category) + ".css")
 
     return render_template(
         'index.html',
     )
 
 
-@app.route("/graph", methods=["GET"])
-def show_graph():
-    return render_template("first_figure.html")
+@app.route("/categories/<string:cat_name>", methods=["GET"])
+def send_cat(cat_name):
+    #print(send_from_directory("application/templates/categories", cat_name + ".html"))
+    print(cat_name)
+    return render_template("categories/" + cat_name + ".html")
+
+
