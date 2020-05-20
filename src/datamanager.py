44be3984b2fe9797aff5fa6bedae9f83442243d4
SmoxBoye <33005387+SmoxBoye@users.noreply.github.com>
2020-05-20 18:20:05 +0200

    Made changes to error handeling

--- src/datamanager.py
+++ src/datamanager.py
@@ -20,36 +20,25 @@ class DataManager:
 
         Args:
             path (string): the path to the csv file.
-
-        Returns:
-            int: error codes; 0: Success, 1: FileNotFoundError, -1: other Errors.
         """  
 
-        # Tries to append path csv as dataframe and send return code.
-        try:
-            with open(path) as f:
-                for i, row in enumerate(f):
-                    if row.startswith("Datum;"):
-                        df = pandas.read_csv(filepath_or_buffer=path, sep=";", skiprows=i)
-                        
-                        df = self._clean_columns(df)
-                        
-                        df.insert(0, "time", df["Datum"] + ":" + df["Tid (UTC)"].str[:2])
-                        df = df.drop(columns=["Datum", "Tid (UTC)"])
-                        #TODO Make categories updatable so if same cat is uploaded the old one gets replaced
-                        for cat in [cat for cat in df.columns if cat != "time"]:
-                            if cat not in self._categories:
-                                single_df = pandas.concat([df["time"], df[cat]], axis=1)
-                                self._dataframes.append(single_df)
-                                self._categories.append(cat)
-                        break
-            return 0
-        except FileNotFoundError:
-            return 1
-        except Exception as e:
-            print(f"\nError: {e} \n")
-            # If unspecified error occurs will return -1.
-            return -1
+        with open(path) as f:
+            for i, row in enumerate(f):
+                if row.startswith("Datum;"):
+                    df = pandas.read_csv(filepath_or_buffer=path, sep=";", skiprows=i)
+                    
+                    df = self._clean_columns(df)
+                    
+                    df.insert(0, "time", df["Datum"] + ":" + df["Tid (UTC)"].str[:2])
+                    df = df.drop(columns=["Datum", "Tid (UTC)"])
+                    #TODO Make categories updatable so if same cat is uploaded the old one gets replaced
+                    for cat in [cat for cat in df.columns if cat != "time"]:
+                        if cat not in self._categories:
+                            single_df = pandas.concat([df["time"], df[cat]], axis=1)
+                            self._dataframes.append(single_df)
+                            self._categories.append(cat)
+                    break
+
     
     @property
     def categories(self):
@@ -60,6 +49,14 @@ class DataManager:
         pass
     
     def get_category(self, category):
+        if type(category) is str:
+            return self.get_single(category)
+        elif type(category) is list:
+            return self.get_multiple(category)
+        else:
+            raise TypeError("get_category only supports string or list objects")
+    
+    def get_single(self, category):
         if category not in self._categories:
             return -1
         
@@ -71,7 +68,7 @@ class DataManager:
         dataframes = []
         
         for cat in categories:
-            dataframes.append(self.get_category(cat))
+            dataframes.append(self.get_single(cat))
         
         multi = dataframes.pop(0)
         for frame in dataframes:
