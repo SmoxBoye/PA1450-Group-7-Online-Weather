da9b5c5bc283a2069778f78dfd9b7d00d9bf3e3e
SmoxBoye <33005387+SmoxBoye@users.noreply.github.com>
2020-05-24 00:53:39 +0200

    Started working on XML loader

--- src/datamanager.py
+++ src/datamanager.py
@@ -1,5 +1,5 @@
 import pandas
-import numpy as np
+import xml.etree.ElementTree as et
 
 class DataManager:
     def __init__(self):
@@ -30,13 +30,7 @@ class DataManager:
             for index, row in df.iterrows():
                 self._cat_unit[row["Parameternamn"]] = row["Enhet"]
     
-    def load_dataframe(self, path):
-        """load_dataframe reads path csv file and adds it as a dataframe into self.dataframes.
-
-        Args:
-            path (string): the path to the csv file.
-        """  
-
+    def load_csv(self, path):
         with open(path) as f:
             for i, row in enumerate(f):
                 if row.startswith("Datum;"):
@@ -58,6 +52,47 @@ class DataManager:
                                     self._dataframes[i] = single_df
                     break
         self._get_unit_from_file(path)
+    
+    def find_and_get(self, node, find, get):
+        try: 
+            return node.find(find).attrib.get(get)
+        except Exception as e:
+            print(e)
+            return None
+    
+    def load_xml(self, path):
+        with open(path) as f:
+            xtree = et.parse(f)
+            
+        xroot = xtree.getroot()
+        
+        root = xroot.find("forecast").find("tabular")
+        rows = []
+        for node in root:
+            time = node.attrib.get("from")
+            persc = self.find_and_get(node, "precipitation", "value")
+            windd = self.find_and_get(node, "windDirection", "deg")
+            winds = self.find_and_get(node, "windSpeed", "mps")
+            temp = self.find_and_get(node, "temperature", "value")
+            pressure = self.find_and_get(node, "temperature", "value")
+            
+            rows.append({"time": time, "precipitation": persc, "windDirection": windd,
+                            "windSpeed": winds, "temperature": temp, "pressure": pressure})
+        
+        df = pandas.DataFrame(rows, columns=[
+                              "time", "precipitation", "windDirection", "windSpeed", "temperature", "pressure"])
+        print(df)
+
+    def load_dataframe(self, path):
+        """load_dataframe reads path csv file and adds it as a dataframe into self.dataframes.
+
+        Args:
+            path (string): the path to the csv file.
+        """ 
+        if path.endswith(".csv"):
+            self.load_csv(path)
+        elif path.endswith(".xml"):
+            self.load_xml(path)
 
     
     @property
